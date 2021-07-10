747b8d949920939f39a8194cafbe174e96b2ae4f
[inga-cae] <[inga.cause.and.effect@gmail.com]>
2021-07-10 15:51:30 +0900

    版数３

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@
 //-----------------------------------------------------------------------------
 images.forEach(function(image) {
     image.addEventListener('click', function() {
-         modalWrapper.classList.add('show');
-         modalImage.classList.add('show');
+        modalWrapper.classList.add('show');
+        modalImage.classList.add('show');
 
-         var imageSrc = image.getAttribute('data-src');
-         modalImage.src = imageSrc;
+        var imageSrc = image.getAttribute('data-src');
+        modalImage.src = imageSrc;
     });
 });
 //-----------------------------------------------------------------------------
@@ -17,23 +17,36 @@ images.forEach(function(image) {
 //-----------------------------------------------------------------------------
 modalWrapper.addEventListener('click', function() {
     if (this.classList.contains('show')) {
-         this.classList.remove('show');
-         modalImage.classList.remove('show');
+        this.classList.remove('show');
+        modalImage.classList.remove('show');
     }
 });
 
 // Bad trial code
-function false_facebook() 
-{
+function false_facebook() {
     alert("老化がひどいSNSに何を共有しようとしているのだ・・・");
 }
 // Bad trial code
-function false_twitter() 
-{
+function false_twitter() {
     alert("アングラなこのサイトを喧伝するとは何事だ・・・・");
 }
 // Bad trial code
-function false_login() 
-{
+function false_login() {
     alert("何を登録するんだよ（笑）");
+}
+// Bad trial code
+function false_HTMLandCSS() {
+    alert("ただで教えるわけなかろう・・・");
+}
+// Bad trial code
+function false_JQuery() {
+    alert("無料で素人は何事か・・・");
+}
+// Bad trial code
+function false_Ruby() {
+    alert("自分で頑張ろうｗｗｗ");
+}
+// Bad trial code
+function false_PHP() {
+    alert("獅子は子を千尋の谷に突き落とすという・・・");
 }
